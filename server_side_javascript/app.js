797d05f3aba167e6ffa7514d9dd59420d334e412
ehhahappy <mqmqmqmqmq@naver.com>
2020-10-01 17:49:19 +0900

    use_URL

--- server_side_javascript/app.js
+++ server_side_javascript/app.js
@@ -3,13 +3,44 @@ var app = express();
 app.locals.pretty = true;
 app.set('view engine', 'pug');
 app.use(express.static('public'));
-app.get('/template', function (req, res){
+app.get('/s_topic/:id',function(req, res){  // 시맨틱 URL 사용
+    var topics = [
+        'Javascript is ...',
+        'Nodejs is ...',
+        'Express is ...'
+    ];
+    var output =`
+        <a href="/s_topic/0">JavaScript</a><br>
+        <a href="/s_topic/1">Nodejs</a><br>
+        <a href="/s_topic/2">Express</a><br><br>
+        ${topics[req.params.id]}
+    `
+    res.send(output)
+})
+app.get("/s_topic/:id/:mode", function(req, res){
+    res.send(req.params.id+','+req.params.mode)
+})
+app.get('/topic',function(req, res){ // 쿼리스트링 사용
+    var topics = [
+        'Javascript is ...',
+        'Nodejs is ...',
+        'Express is ...'
+    ];
+    var output =`
+        <a href="/topic?id=0">JavaScript</a><br>
+        <a href="/topic?id=1">Nodejs</a><br>
+        <a href="/topic?id=2">Express</a><br><br>
+        ${topics[req.query.id]}
+    `
+    res.send(output)
+})
+app.get('/template', function (req, res){  // pug 패키지 사용(view폴더)
     res.render('index', {time:Date()});
 })
-app.get('/', function(req, res){
+app.get('/', function(req, res){  // home dir
     res.send('Hello home page');
 });
-app.get('/dynamic', function(req, res){
+app.get('/dynamic', function(req, res){  // 동적 페이지
     var lis = ''
     for(var i=0; i<5; i++){
         lis = lis + '<li>coding</li>'
@@ -32,10 +63,10 @@ app.get('/dynamic', function(req, res){
 </html>`
     res.send(output)
 })
-app.get('/route', function(req, res){
+app.get('/route', function(req, res){ // 이미지 출력
     res.send('Hello Router, <img src="/whale.png">')
 })
-app.get('/login', function(req, res){
+app.get('/login', function(req, res){ // 글자 출력
     res.send('login plese')
 })
 app.listen(3000, function(){
