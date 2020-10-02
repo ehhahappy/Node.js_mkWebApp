15b7de238845abcc92b3c943aaa4e9d308cc1f66
ehhahappy <mqmqmqmqmq@naver.com>
2020-10-02 21:28:32 +0900

    user_getORpost

--- server_side_javascript/app.js
+++ server_side_javascript/app.js
@@ -1,8 +1,26 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
 app.locals.pretty = true;
 app.set('view engine', 'pug');
 app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.get('/form_get', function(req, res){  // 정보전달(get 방식)
+    res.render('form_get');
+});
+app.get('/form_receiver', function(req, res){
+    var title = req.query.title;
+    var description = req.query.description;
+    res.send(title+','+description)
+});
+app.get('/form_post', function(req, res){ // 정보전달(post 방식)
+    res.render('form_post');
+});
+app.post('/form_receiver', function(req, res){
+    var title = req.body.title;
+    var description = req.body.description;
+    res.send(title+','+description);
+});
 app.get('/s_topic/:id',function(req, res){  // 시맨틱 URL 사용
     var topics = [
         'Javascript is ...',
@@ -16,10 +34,10 @@ app.get('/s_topic/:id',function(req, res){  // 시맨틱 URL 사용
         ${topics[req.params.id]}
     `
     res.send(output)
-})
-app.get("/s_topic/:id/:mode", function(req, res){
+});
+app.get("/s_topic/:id/:mode", function(req, res){  // 두가지 변수를 받음
     res.send(req.params.id+','+req.params.mode)
-})
+});
 app.get('/topic',function(req, res){ // 쿼리스트링 사용
     var topics = [
         'Javascript is ...',
@@ -33,10 +51,10 @@ app.get('/topic',function(req, res){ // 쿼리스트링 사용
         ${topics[req.query.id]}
     `
     res.send(output)
-})
+});
 app.get('/template', function (req, res){  // pug 패키지 사용(view폴더)
     res.render('index', {time:Date()});
-})
+});
 app.get('/', function(req, res){  // home dir
     res.send('Hello home page');
 });
@@ -61,14 +79,14 @@ app.get('/dynamic', function(req, res){  // 동적 페이지
     ${time}
 </body>
 </html>`
-    res.send(output)
-})
+    res.send(output);
+});
 app.get('/route', function(req, res){ // 이미지 출력
-    res.send('Hello Router, <img src="/whale.png">')
-})
+    res.send('Hello Router, <img src="/whale.png">');
+});
 app.get('/login', function(req, res){ // 글자 출력
-    res.send('login plese')
-})
+    res.send('login plese');
+});
 app.listen(3000, function(){
     console.log('Conneted 3000 port!');
 });
