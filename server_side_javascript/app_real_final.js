11a044f31b7fb2f89cfdb56f5fc6d81a4c7ddfb7
ehhahappy <mqmqmqmqmq@naver.com>
2020-12-25 19:19:31 +0900

    Fix typo

--- server_side_javascript/app_real_final.js
+++ server_side_javascript/app_real_final.js
@@ -1,7 +1,7 @@
 var app = require('./real_final_config/express')();
 var passport = require('./real_final_config/passport')(app);
 var auth = require('./real_final_routes/auth')(passport);
-app.use('/auth', auth);
+app.use('/auth', auth); 
 
 var topic = require('./real_final_routes/topic')();
 app.use('/topic', topic);
